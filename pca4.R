ee1af152b90884196fb73405e807210136196893
estepi <soyestepi@gmail.com>
2022-04-21 18:58:36 +0200

    pca 4

--- pca4.R
+++ pca4.R
@@ -30,3 +30,42 @@ exportIntegratedSignals(is,sr=sr,
                           output.dir = "aspliExample",
                           counts=gbcounts,features=features,asd=asd,
                           mergedBams = mBAMs)
+
+#######################################
+library(ASpli)
+library(GenomicFeatures)
+
+# gtf preprocessing ----
+gtfFileName <- aspliExampleGTF()
+genomeTxDb  <- makeTxDbFromGFF( gtfFileName )
+
+# feature extraction ----
+features    <- binGenome( genomeTxDb )
+#bams and target file ----
+BAMFiles <- aspliExampleBamList()
+targets  <- data.frame(row.names = paste0('Sample',c(1:6)),
+                       bam = BAMFiles[1:6],
+                       f1  = c( 'control','control','control','treatment','treatment','treatment'),
+                       stringsAsFactors = FALSE)
+mBAMs <- data.frame( bam = sub("_[012]","",targets$bam[c(1,4)]),
+                     condition = c("control","treatment"))
+
+gbcounts <- gbCounts(features=features, targets=targets,
+                     minReadLength = 100, maxISize = 50000)
+gbcounts
+
+asd <- jCounts(counts=gbcounts, features=features, minReadLength=100)
+asd
+gb  <- gbDUreport(gbcounts, contrast = c(-1,1))
+gb
+jdur <- jDUreport(asd, contrast=c(-1,1))
+jdur
+
+sr <- splicingReport(gb, jdur, counts=gbcounts)
+is <- integrateSignals(sr,asd)
+exportIntegratedSignals(is,sr=sr,
+                        output.dir = "aspliExample",
+                        counts=gbcounts,features=features,asd=asd,
+                        mergedBams = mBAMs)
+
+getwd()
